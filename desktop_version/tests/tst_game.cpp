31b5686b0d4f930be4be374249e3aa55dcc67956
curaai00 <curaai00@gmail.com>
2020-08-25 10:04:31 +0900

    chore: Add googletest as submodule
    make runnable on windows environment

--- desktop_version/tests/tst_game.cpp
+++ desktop_version/tests/tst_game.cpp
@@ -1,32 +1,9 @@
 #include "../src/Game.h"
 #include "gtest/gtest.h"
 
-// class TestGame : public ::testing::Test
-// {
-// public:
-//     TestGame()
-//         : Test()
-//     {
-//     }
-
-// protected:
-//     virtual void SetUp()
-//     {
-//     }
-
-// };
-
-// TEST_F(TestGame, string2button) {
-//     SDL_GameControllerButton exp_btn;
-//     auto res = GetButtonFromString("0", &exp_btn);
-//     EXPECT_EQ(exp_btn, SDL_CONTROLLER_BUTTON_A);
-//     EXPECT_EQ(1, 1);
-// }
-
-
 TEST(TestGame, string2button) {
-    SDL_GameControllerButton exp_btn;
+    SDL_GameControllerButton exp_btn = SDL_GameControllerButton::SDL_CONTROLLER_BUTTON_INVALID;
     auto res = GetButtonFromString("0", &exp_btn);
+    EXPECT_TRUE(res);
     EXPECT_EQ(exp_btn, SDL_CONTROLLER_BUTTON_A);
-    EXPECT_EQ(1, 1);
 }
